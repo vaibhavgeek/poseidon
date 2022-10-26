98f142b5038bfb5e465f564bf16d4ce782985a77
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2022-10-26 09:06:44 +0530

    complete constants in noir

--- src/file.py
+++
@@ -1,6 +0,0 @@
-import re
-f = open("constants_c.circom","r")
-file = f.read()
-get_constants = re.findall(r'(\[(?:\[??[^\[]*?\]))', file)
-#results  = get_constants.search(file)
-print(get_constants[1])
